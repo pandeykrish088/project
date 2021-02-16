708f75ac4fa5736e2d784a0aad6b8028b7f8a99b
pandeykrish088 <76039564+pandeykrish088@users.noreply.github.com>
2021-02-16 12:57:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var gameState = 0;
+var playerCount;
+var form,player,game;
+var database;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+    
+}
+
+function draw(){
+
+}
+
